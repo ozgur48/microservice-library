fe786a61f97bafbe18cd10f04df44a0b9087a86c
Özgür Durak <ozgur.durak@yandex.com>
2025-11-01 21:34:35 +0300

    gateway config updated

---
+++ gateway-server/src/main/java/com/turkcell/gateway_server/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.turkcell.gateway_server.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class GatewayConfig {
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route("book-service", r -> r
+                        .path("/api/v1/books/**")
+                        .filters(f -> f.retry(config -> config.setRetries(3)))
+                        .uri("lb://book-service"))
+                .route("fallback", r -> r
+                        .path("/**")
+                        .filters(f -> f.setStatus(HttpStatus.NOT_FOUND))
+                        .uri("no://op"))
+                .build();
+    }
+}
