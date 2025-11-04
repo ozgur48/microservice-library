5b5a2b6c875c42e19c7b610645ebe49da9065342
Özgür Durak <ozgur.durak@yandex.com>
2025-11-04 09:15:48 +0300

    author-config updated. Docker compose updated

--- book-service/src/main/java/com/turkcell/book_service/interfaces/web/BooksController.java
+++ book-service/src/main/java/com/turkcell/book_service/interfaces/web/BooksController.java
@@ -4,6 +4,7 @@ import com.turkcell.book_service.application.book.command.CreateBookCommand;
 import com.turkcell.book_service.application.book.dto.CreatedBookResponse;
 import com.turkcell.book_service.cqrs.CommandHandler;
 import jakarta.validation.Valid;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -13,15 +14,21 @@ import org.springframework.web.bind.annotation.*;
 @Validated
 public class BooksController {
     private final CommandHandler<CreateBookCommand, CreatedBookResponse> createBookCommandHandler;
+    private final StreamBridge streamBridge;
 
-    public BooksController(CommandHandler<CreateBookCommand, CreatedBookResponse> createBookCommandHandler) {
+
+    public BooksController(CommandHandler<CreateBookCommand, CreatedBookResponse> createBookCommandHandler, StreamBridge streamBridge) {
         this.createBookCommandHandler = createBookCommandHandler;
+        this.streamBridge = streamBridge;
     }
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CreatedBookResponse createBook(@Valid @RequestBody CreateBookCommand command){
-        return createBookCommandHandler.handle(command);
+        CreatedBookResponse book = createBookCommandHandler.handle(command);
+        streamBridge.send("bookCreated-out", book.title());
+        return book;
     }
+
     @GetMapping
     public String getBook(){
         return "merhaba";
