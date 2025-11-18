46e6e7b44febc55c823ee511fe59d92cf1f28cc8
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-11-18 14:54:01 +0300

    add an import

--- staff-service/src/main/java/com/turkcell/staff_service/application/command/DeleteStaffCommand.java
+++ staff-service/src/main/java/com/turkcell/staff_service/application/command/DeleteStaffCommand.java
@@ -1,6 +1,7 @@
 package com.turkcell.staff_service.application.command;
 
 import com.turkcell.staff_service.application.dto.DeletedStaffResponse;
+import com.turkcell.staff_service.cqrs.Command;
 import jakarta.validation.constraints.NotBlank;
 
 import java.util.UUID;
