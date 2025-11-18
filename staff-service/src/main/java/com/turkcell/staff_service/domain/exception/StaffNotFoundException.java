ff9ac0437f09411734dbb2b6ecdacb11c8245e50
Eda Akkaya <edaakkaya12@hotmail.com.tr>
2025-11-18 14:40:24 +0300

    add domain exception and port

--- staff-service/src/main/java/com/turkcell/staff_service/domain/exception/StaffNotFoundException.java
+++ staff-service/src/main/java/com/turkcell/staff_service/domain/exception/StaffNotFoundException.java
@@ -1,7 +1,16 @@
 package com.turkcell.staff_service.domain.exception;
 
+import com.turkcell.staff_service.domain.model.StaffId;
+
+import java.util.UUID;
+
 public class StaffNotFoundException extends RuntimeException {
-  public StaffNotFoundException(String message) {
-    super(message);
-  }
+    public StaffNotFoundException(UUID id) {
+        super("Staff with id: " + id + " could not find!");
+    }
+
+    public StaffNotFoundException(StaffId id) {
+        super("Staff with id: " + id + " could not find!");
+    }
+
 }
--- staff-service/src/main/java/com/turkcell/staff_service/domain/repository/StaffRepository.java
+++ staff-service/src/main/java/com/turkcell/staff_service/domain/repository/StaffRepository.java
@@ -6,7 +6,7 @@ import com.turkcell.staff_service.domain.model.StaffId;
 import java.util.List;
 import java.util.Optional;
 
-public interface StaffInterface {
+public interface StaffRepository {
     Staff save(Staff staff);
     Optional<Staff> findById(StaffId staffId);
     List<Staff> findAll();
